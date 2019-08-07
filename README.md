3bae5e1480f9589f91558b72254a59e6e4fce0bd
Petter Fors <karl.petter.andersson@gmail.com>
2019-08-07 19:28:58 +0200

    Change from ENV  variabel to ini file

--- README.md
+++ README.md
@@ -4,12 +4,11 @@
 # testbed_from_inventory
 this module add a function to create a pyats testbed object from a ansible inventory.
 
-# envioment variables
-spesify the two vaiables, it's mndatory for the creation of the tesbed
-- PYATS_USERNAME
-- PYATS_PASSWORD
+# credential - username/password
+the credentials are provided to the "config.ini" file
+simply rename the config_template.ini and modify to your need 
 
 # To run the test
-'easypy version_check_job.py'
+´easypy version_check_job.py´
 ## with html report
-easypy version_check_job.py -html_logs html_logs/
+´easypy version_check_job.py -html_logs html_logs/´
--- testbed_from_inventory.py
+++ testbed_from_inventory.py
@@ -3,30 +3,42 @@ import pprint
 import os.path
 from sh import Command
 from genie.conf import Genie
-from genie.libs import ops # noqa
+from genie.libs import ops  # noqa
 from pyats.topology import Testbed, Device
+import configparser
 
-if 'PYATS_USERNAME' in os.environ:
-    tacacs_user = os.environ.get('PYATS_USERNAME')
-else:
-    raise AssertionError('you are missing environmen variabel')
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-if 'PYATS_PASSWORD' in os.environ:
-    tacacs_password = os.environ.get('PYATS_PASSWORD')
-else:
-    raise AssertionError('you are missing environmen variabel')
-    
+tacacs_user = config.get("DEFAULT", "PYATS_USERNAME")
+tacacs_password = config.get("DEFAULT", "PYATS_PASSWORD")
+
+#
+# if 'PYATS_USERNAME' in os.environ:
+#    tacacs_user = os.environ.get('PYATS_USERNAME')
+# else:
+#    raise AssertionError('you are missing environmen variabel')
+#
+# if 'PYATS_PASSWORD' in os.environ:
+#    tacacs_password = os.environ.get('PYATS_PASSWORD')
+# else:
+#    raise AssertionError('you are missing environmen variabel')
+#
+#
+
 
 def get_ansibel_inventory(inventory_path):
     """Return full Ansible object `inventory_path`."""
-    
+
     if not os.path.exists(inventory_path):
-        raise AssertionError('File %r do not exist, check your path to the Ansible inventory file' % inventory_path)
-    
+        raise AssertionError(
+            'File %r do not exist, check your path to the Ansible inventory file' % inventory_path)
+
     ansible_inventory = Command('ansible-inventory')
     json_inventory = json.loads(
         ansible_inventory('-i', inventory_path, '--list').stdout)
     return json_inventory
+
 
 def get_hosts_from(inventory_path, group_name):
     """Return list of hosts from `group_name` in Ansible `inventory_path`."""
@@ -55,43 +67,44 @@ def get_hosts_from(inventory_path, group_name):
                             hosts.append(host)
         return hosts
 
+
 def get_hostvars(host, inventory):
     """Retruns host vars from a specific host"""
     host_vars = inventory['_meta']['hostvars'][host]
     return host_vars
-    
-    
+
+
 def create_testbed(inventory_path, group_name):
     """Returns a pyats tesbed object"""
     hosts = get_hosts_from(inventory_path, group_name)
     testbed_obj = Testbed('my_testbed')
-    
+
     device_list = []
     for host in hosts:
-        host_vars = get_hostvars(host, 
+        host_vars = get_hostvars(host,
                                  get_ansibel_inventory(inventory_path)
                                  )
         dev = Device(host,
-                    type =  'ios',
-                    os =  'ios',
-                    tacacs={
-                        'username': os.environ.get('PYATS_USERNAME'),
-                        'login_prompt': 'login as:',
+                     type='ios',
+                     os='ios',
+                     tacacs={
+                         'username': tacacs_user,
+                         'login_prompt': 'login as:',
                         'password_prompt': 'Password:',
-                    },
-                    passwords={
-                        'tacacs': os.environ.get('PYATS_PASSWORD'),
-                        'enable': 'lab',
-                        'line': 'lab',
-                    },
-                    connections =  {
-                        'ssh': { 
-                            'protocol' : 'ssh',
-                            'ip': host_vars['mgmtip']
-                            }
-                    })
-        dev.os =  "ios"
+                     },
+                     passwords={
+                         'tacacs': tacacs_password,
+                         'enable': 'lab',
+                         'line': 'lab',
+                     },
+                     connections={
+                         'ssh': {
+                             'protocol': 'ssh',
+                             'ip': host_vars['mgmtip']
+                         }
+                     })
+        dev.os = "ios"
         testbed_obj.add_device(dev)
-        
-    testbed = Genie.init(testbed_obj)    
+
+    testbed = Genie.init(testbed_obj)
     return testbed
